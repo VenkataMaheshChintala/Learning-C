a97a1c5adb259a10e6ddd6f841b5dae5879e80f4
Venkata Mahesh Chintala <venkatamaheshchintala@gmail.com>
2023-12-03 14:39:13 +0530

    transpose

--- transpose_of_n*n_matrix.c
+++ transpose_of_n*n_matrix.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 int main()
 {
-    int m,n;
-    printf("Enter the no.of rows and columns in the matrix : ");
-    scanf("%d %d",&m,&n);
-    int a[m][n],t=0,s=0;
+    int m;
+    printf("Enter the no.of rows/columns in the matrix : ");
+    scanf("%d",&m);
+    int a[m][m],t=0,s=0;
     printf("Enter the elements in the matrix : ");
-    for(int i=0;i<m;i++) for(int j=0;j<n;j++) scanf("%d",&a[i][j]);
+    for(int i=0;i<m;i++) for(int j=0;j<m;j++) scanf("%d",&a[i][j]);
     for(int i=0;i<m;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<m;j++)
         {
             t = a[i][j];
             s = a[j][i];
@@ -19,7 +19,7 @@ int main()
         }
     }
     printf("The resultant matrix is : \n");
-    for(int i=0;i<n;i++)
+    for(int i=0;i<m;i++)
     {
         for(int j=0;j<m;j++) printf("%d ",a[j][i]);
         printf("\n");
